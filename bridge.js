0d0c5526ec44e338dd400861b5e91407346fd726
Vince Elizaga <vncelizaga@gmail.com>
2017-04-28 12:05:06 +0800

    add: mqtt channel

--- bridge.js
+++ bridge.js
@@ -24,12 +24,13 @@ sensorClient.on('connect', function () {
   console.log('new thing connection');
 });
 
-sensorClient.subscribe('v1/messages/#');
+sensorClient.subscribe('#');
 tfssClient.subscribe('v1/actions/#');
 
 // listen to messages coming from the sensor mqtt broker
 sensorClient.on('message', function (topic, payload, packet) {
-  console.log(topic + ' : ' + payload);
+  console.log(packet);
+  // console.log('received: ' + topic + ' : ' + payload);
   var info = String(topic);
   info = info.split('/');
 
@@ -41,17 +42,41 @@ sensorClient.on('message', function (topic, payload, packet) {
 
   if(message.type === 'messages'){
     // 1. authenticate message ( TODO )
+    if(authenticateMessage(message)){
+      // 2. save the data log to nosql db
+      dataLogger.save(message.deviceId, String(payload), function(result){
+      }, function(error){
+        console.log(error);
+      });         
 
-    // 2. save the data log to nosql db
-    // dataLogger.save(message.deviceId, String(payload), function(result){
-    //   console.log(result);
-    // }, function(error){
-    //   console.log(error);
-    // });
-    // 3. pass this message to tf webserver`
-    console.log('transmitting to TF WebServer: ', payload);
-    tfssClient.publish(topic, payload);
+      // 3. pass this message to tf webserver`
+      console.log('transmitting to TF WebServer: ', payload.toString());
+      tfssClient.publish(topic, payload);      
+    } else {
+      // if message has invalid token, send back error message 
+      var callbackTopic = 'v1/errors/<deviceId>';
+      sensorClient.publish(callbackTopic, callbackPayload);
+    }
+  } else if(message.type === 'actions'){
+    // process messages in 'action'type
+  } else{
+    console.log('invalid path');
+    sensorClient.unsubscribe('#');
+    if(topic === '1004'){
+      sensorClient.publish('v1/errors/1004', '403:' + topic + ':invalid path');
+    }else{
+      sensorClient.publish('v1/errors/1003', '403:' + topic + ':invalid path');
+    }
+    
+    sensorClient.subscribe('#');
   }
 
 });
+
+function authenticateMessage(message){
+  // 1. look up in the database if session token exists
+  // 2. if valid, decode the token and check the sender
+
+  return true;
+}
 
